d2f23d4912546094fa385679963db9fa12c911b0
renz <70139212+renzjared@users.noreply.github.com>
2026-09-01 00:56:20 +0800

    more fixes

--- js/app.js
+++ js/app.js
@@ -183,7 +183,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js').catch(err => console.error("SW Registration failed: ", err));
+            navigator.serviceWorker.register('./sw.js').catch(err => console.error("SW Registration failed: ", err));
         });
     }
 });
